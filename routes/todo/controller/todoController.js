f68957f25c23f86ea99fb8f2476fbde1c7c62a9c
spaceforestchu <spaceforestchu@gmail.com>
2021-06-15 16:11:39 -0400

    completed

--- routes/todo/controller/todoController.js
+++ routes/todo/controller/todoController.js
@@ -3,6 +3,7 @@ const Todo = require("../model/Todo");
 async function getAllTodos(req, res) {
   try {
     let allTodos = await Todo.find({});
+
     res.json({ payload: allTodos });
   } catch (e) {
     res.status(500).json({ message: e.message, error: e });
@@ -10,6 +11,8 @@ async function getAllTodos(req, res) {
 }
 
 async function createTodo(req, res) {
+  console.log(req.body);
+
   try {
     let createdTodo = new Todo({
       todo: req.body.todo,
@@ -49,9 +52,61 @@ async function deleteTodo(req, res) {
   }
 }
 
+async function updateTodoDone(req, res) {
+  try {
+    let updatedTodoDone = await Todo.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json({ payload: updatedTodoDone });
+  } catch (e) {
+    res.status(500).json({ message: e.message, error: e });
+  }
+}
+
+async function sortTodoByDate(req, res) {
+  try {
+    let sort = req.query.sort;
+    let sortOrder = sort === "desc" ? -1 : 1;
+
+    let foundTodo = await Todo.find({}).sort({ dateAdded: sortOrder });
+
+    res.json({ payload: foundTodo });
+  } catch (e) {
+    res.status(500).json({ message: e.message, error: e });
+  }
+}
+
+async function sortTodoByDone(req, res) {
+  try {
+    let isDone = req.query.isDone;
+    let isDoneOrder = isDone === "true" ? true : false;
+
+    let sortByDate = req.query.sort ? req.query.sort : null;
+    let finalSort;
+    if (!sortByDate) {
+      finalSort = null;
+    } else {
+      finalSort = sortByDate === "asc" ? 1 : -1;
+    }
+
+    let foundTodo = await Todo.find({ isDone: isDoneOrder }).sort({
+      dateAdded: finalSort,
+    });
+
+    res.json({ payload: foundTodo });
+  } catch (e) {
+    res.status(500).json({ message: e.message, error: e });
+  }
+}
+
 module.exports = {
   getAllTodos,
   createTodo,
   updateTodo,
   deleteTodo,
+  updateTodoDone,
+  sortTodoByDate,
+  sortTodoByDone,
 };
--- routes/todo/todoRouter.js
+++ routes/todo/todoRouter.js
@@ -6,6 +6,9 @@ const {
   createTodo,
   updateTodo,
   deleteTodo,
+  updateTodoDone,
+  sortTodoByDate,
+  sortTodoByDone,
 } = require("./controller/todoController");
 
 /* GET home page. */
@@ -15,10 +18,20 @@ router.get("/", function (req, res, next) {
 
 router.get("/get-all-todos", getAllTodos);
 
+router.get("/get-todos-by-sort", sortTodoByDate);
+
+router.get("/get-todos-by-done", sortTodoByDone);
+
 router.post("/create-todo", createTodo);
 
 router.put("/update-todo-by-id/:id", updateTodo);
 
+router.put("/update-done-by-id/:id", updateTodoDone);
+
 router.delete("/delete-todo-by-id/:id", deleteTodo);
 
 module.exports = router;
+
+//create a PUT path that update isDone
+//get it to work using postman first
+//hook it up in the frontend side
